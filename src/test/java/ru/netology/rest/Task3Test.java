6cd3016a21cd54be9619beda7b52a7f2e3c8d2aa
Ivan Sibiryakov <ivan.sibiryakov27@gmail.com>
2023-11-19 15:13:58 +0300

    return correct checking to test

--- src/test/java/ru/netology/rest/Task3Test.java
+++ src/test/java/ru/netology/rest/Task3Test.java
@@ -19,7 +19,7 @@ public class Task3Test {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data spoiled"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
